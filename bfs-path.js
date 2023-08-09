6d2494e153f0f8934fd2bf5f8b50a49952d76e10
John <jjpyae@gmail.com>
2023-08-09 14:37:58 -0700

    completes findNeighbors

--- bfs-path.js
+++ bfs-path.js
@@ -1,13 +1,21 @@
 function findNeighbors(node, matrix) {
+    const row = node[0];
+    const col = node[1];
+    const neighbors = [];
+
     // Up
+    if (row - 1 >= 0) neighbors.push([row - 1, col]);
 
     // Down
+    if (row + 1 < matrix.length) neighbors.push([row + 1, col]);
 
     // Left
+    if (col - 1 >= 0) neighbors.push([row, col - 1]);
 
     // Right
+    if (col + 1 < matrix[row].length) neighbors.push([row, col + 1]);
 
-    // Your code here
+    return neighbors;
 }
 
 
@@ -18,7 +26,7 @@ function bfsPath(matrix, startNode, endValue) {
 
 // ***** UNCOMMENT FOR LOCAL TESTING *****
 
-// const matrix1 = [ 
+// const matrix1 = [
 //     [  1,  2,  3,  4 ],
 //     [  5,  6,  7,  8 ],
 //     [  9, 10, 11, 12 ],
@@ -60,11 +68,11 @@ function bfsPath(matrix, startNode, endValue) {
 // // value is located at start node
 // // [ [ 2, 2 ] ]
 
-// console.log(bfsPath(matrix1, [1,2], 8)); // can handle various start nodes 
+// console.log(bfsPath(matrix1, [1,2], 8)); // can handle various start nodes
 // // and end values
 // // [ [ 1, 2 ], [ 0, 2 ], [ 2, 2 ], [ 1, 1 ], [ 1, 3 ] ]
 
-// console.log(bfsPath(matrix1, [0,0], 17)); // can return false if end value 
+// console.log(bfsPath(matrix1, [0,0], 17)); // can return false if end value
 // // is not found
 // // false
 
